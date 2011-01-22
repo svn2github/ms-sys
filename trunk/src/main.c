f1a2caec5692add9ea60c6b8a35cbb39fe520167
henca <henca@61900cd2-630f-449f-a399-a8f8fe48ea5b>
2011-01-22 17:08:13 +0000

    Now doing sanity check before only writing BPB
    git-svn-id: https://svn.code.sf.net/p/ms-sys/code2@82 61900cd2-630f-449f-a399-a8f8fe48ea5b

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -87,6 +87,14 @@ int main(int argc, char **argv)
    }
    if( bWritePartitionInfo )
    {
+      if( !iBr && !bForce)
+      {
+	 if( ! sanity_check(fp, argv[argc-1], FAT32_BR, 1) )
+	 {
+	    fclose(fp);
+	    return 1;
+	 }
+      }
       if( write_partition_start_sector_number(fp) )
       {
 	 printf(_("Start sector %ld (nr of hidden sectors) successfully written to %s\n"),
